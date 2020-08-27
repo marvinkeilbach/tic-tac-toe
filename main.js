ad1e2ff1b42330cce442d0234f3daf59f24a703d
Marvin Keilbach <marvinkeilbach@protonmail.com>
2020-08-27 11:17:28 +0200

    Full functionality for 2 human players

--- main.js
+++ main.js
@@ -1,55 +1,68 @@
 // Tasks: 
-// work out the checkwin and checkdraw methods and create a "new game" method
-// create a transitioning popup on the display with initial comments "you are green, computer is blue, good luck!", which fades out
-// when tie or won, transition in a new popup, and ask if a new game is wished -> only when it is clicked, let it fade out (don't allow DOM manipulation when it is still there)
-// if a new game is wished, let a new popup come in which says "Round xyz"
-// Create color coded score measure (1 point = 1 won round)
-// Find a way to let only the div being clicked without using z-index (OR: find out more how to use z-index)
-// Why? -> You want to be able to set a background, which isn't possible right now
-// Make it beautiful <3
+// Build AI
+// Make reset button
+// Allow choice between 2player game and game against AI
 
 const gameboard = (function() {
-    const _gameboard = [[], [], [], 
-                        [], [], [], 
-                        [], [], []];
+    let _gameboard = [[], [], [], 
+                      [], [], [], 
+                      [], [], []];
+
     const _render = () => {
         fields.forEach((field, index) => {
             field.firstChild.innerText = _gameboard[index][0];
             field.firstChild.classList = `${_gameboard[index][1]}`;
-        })
+        });
     };
+    const updateScore = (winner) => {
+        _humanScore.firstChild.innerText = human.score;
+        _computerScore.firstChild.innerText = computer.score;
+        if(winner === "human") {
+            _humanScore.firstChild.classList.toggle('update-score');
+            _humanScore.classList.toggle('update-score');
+        } else if (winner === "computer") {
+            _computerScore.firstChild.classList.toggle('update-score');
+            _computerScore.classList.toggle('update-score');
+        }
+    }
     const applyMove = (symbol, location, color) => {
         _gameboard[location] = [symbol, color];
+        toggleActive();
         _render();
     };
     const resetGameboard = () => {
         _gameboard.forEach((field, index) => {
             _gameboard[index] = ["", "blank"];
-        })
+        });
+        _render();
     }
-    const _checkStatus = () => {};
-    const fields = document.querySelectorAll('.field');
-    resetGameboard();
-    _render();
+    const toggleActive = () => {
+        _humanScore.classList.toggle('active');
+        _computerScore.classList.toggle('active');
+    }
 
-    return {applyMove, fields};
+    const infoField = document.querySelector('button');
+    const fields = document.querySelectorAll('.field');
+    const _humanScore = document.querySelector('.human-score');
+    const _computerScore = document.querySelector('.computer-score');
+    let status = "pause";
+    return {applyMove, fields, infoField, status, resetGameboard, updateScore, toggleActive};
 })();
 
 const Player = (name, moveStatus) => {
     const _name = name;
     let _moveStatus = moveStatus;
-    const toggleMoveStatus = () => {
-        _moveStatus = !_moveStatus;
-    };
+    const toggleMoveStatus = () => {_moveStatus = !_moveStatus;};
     const checkMoveStatus = () => _moveStatus;
+    const score = 0;
 
-    return {checkMoveStatus, toggleMoveStatus};
+    return {checkMoveStatus, toggleMoveStatus, moveStatus, score};
 }
 
 const gameLogic = (function() {
-    const _winningPatterns = [[0, 1, 2], [0, 3, 5], [0, 4, 8], [1, 4, 7], [2, 4, 6], [2, 5, 8], [3, 4, 5]];
-    const _humanStatus = [];
-    const _computerStatus = [];
+    const _winningPatterns = [[0, 1, 2], [0, 3, 6], [0, 4, 8], [1, 4, 7], [2, 4, 6], [2, 5, 8], [3, 4, 5]];
+    let _humanStatus = [];
+    let _computerStatus = [];
     const applyMove = (name, location) => {
         if(name === "human") {
             console.log(name + location);
@@ -66,7 +79,10 @@ const gameLogic = (function() {
             gameboard.applyMove("O", location, "blue");  
         }
         _checkWin();
-        _checkDraw();
+        if(!winStatus) {
+            _checkDraw();
+        }
+
     };
     const checkMove = (location) => {
         if(_humanStatus.includes(+location) || _computerStatus.includes(+location)) {
@@ -74,6 +90,8 @@ const gameLogic = (function() {
         } else {return true;}
     };
     
+    let winStatus = false;
+
     const _checkWin = () => {
         _winningPatterns.forEach(pattern => {
             let humanCheck = 0;
@@ -87,58 +105,93 @@ const gameLogic = (function() {
                 }
             })
             if(humanCheck === 3) {
-                console.log("yes");
                 alertWin("human");
+                winStatus = true;
             }
             if(computerCheck === 3) {
                 alertWin("computer");
+                winStatus = true;
             }
         })
     }
 
     const _checkDraw = () => {
         if(_humanStatus.length + _computerStatus.length === 9) {
-            alertDraw();
+            alertWin("nobody");
         }
     }
 
     const alertWin = (winner) => {
-        // render alert message on the page
-        // reset the game
-    }
-
-    const alertDraw = () => {
-        // render alert message on the page
-        // reset the game
-    }
-
-    const currentPlayer = () => {
-        if(human.checkMoveStatus === true) {
-            return "human";
+        gameboard.status = "pause";
+        if(winner === "human") {
+            human.score++;
         } else {
-            return "computer";
+            computer.score++
+        }
+        if(winner !== "nobody") {gameboard.infoField.innerText = `the ${winner} won!`;
+        } else {
+            gameboard.infoField.innerText = `that's a tie!`;
+        }
+        gameboard.infoField.parentElement.classList.toggle(`${winner}`);
+        gameboard.infoField.parentElement.classList.toggle('hover');
+        gameboard.infoField.parentElement.classList.toggle('float-visible');
+        setTimeout(function() {
+            gameboard.updateScore(winner);
+        }, 500);
+        setTimeout(function() {
+            gameboard.infoField.innerText = "click to start new round";
+            gameboard.infoField.parentElement.classList.toggle(`${winner}`);
+            gameboard.infoField.parentElement.classList.toggle('hover');
+            gameboard.updateScore(winner);
+        }, 2000);
+
+        // reset the game
+    }
+
+    const reset = () => {
+        _humanStatus = [];
+        _computerStatus = [];
+        winStatus = false;
+        if(!human.checkMoveStatus()) {
+            human.toggleMoveStatus();
+            computer.toggleMoveStatus();
+            gameboard.toggleActive();
         }
     }
 
-    return {checkMove, applyMove, currentPlayer};
+    return {checkMove, applyMove, reset};
 })();
 
 // Event listeners on the gameboard fields
 (function() {
     gameboard.fields.forEach(field => field.addEventListener("click", _fireEvent));
+    gameboard.infoField.addEventListener("click", startGame);
+
     function _fireEvent(e) {
-        const currentMove = e.target.dataset.gridIndex;
-        console.log(e);
-        console.log(human.checkMoveStatus(), gameLogic.checkMove(currentMove));
-        if(human.checkMoveStatus() && gameLogic.checkMove(currentMove)) {
-            gameLogic.applyMove("human", currentMove);
-        } else if(computer.checkMoveStatus() && gameLogic.checkMove(currentMove)) {
-            gameLogic.applyMove("computer", currentMove);
-        } else {
-            alert("This field has already been played.");
+        if(gameboard.status === "running") {
+            const currentMove = e.target.dataset.gridIndex;
+            if(human.checkMoveStatus() && gameLogic.checkMove(currentMove)) {
+                gameLogic.applyMove("human", currentMove);
+            } else if(computer.checkMoveStatus() && gameLogic.checkMove(currentMove)) {
+                gameLogic.applyMove("computer", currentMove);
+            } else {
+                alert("This field has already been played.");
+            }
+        }
+    }
+
+    function startGame(e) {
+        if(gameboard.status === "pause" && gameboard.infoField.innerText === "click to start new round") {
+            e.target.parentElement.classList.toggle('float-visible');
+            gameboard.status = "running"
+            gameLogic.reset();
+            gameboard.resetGameboard();
         }
     }
 })();
 
 const computer = Player("computer", false);
 const human = Player("human", true);
+
+gameboard.resetGameboard();
+gameboard.updateScore();
